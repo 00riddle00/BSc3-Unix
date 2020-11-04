00a772b4cded8036e996bfa7600663d42a61abf5
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2020-11-04 14:34:11 +0200

    basic error handling

--- LAB2/main.c
+++ LAB2/main.c
@@ -36,6 +36,11 @@ char* read_line(const char *prompt) {
 
 char **get_input(char *input) {
     char **command = malloc(CMD_BUFF_SIZE * sizeof(char *));
+    if (command == NULL) {
+        perror("malloc failed");
+        exit(1);
+    }
+
     char *separator = " ";
     char *parsed;
     int index = 0;
@@ -65,9 +70,18 @@ int main() {
         command = get_input(input);
 
         child_pid = fork();
+        if (child_pid <0) {
+            perror("Fork failed");
+            exit(1);
+        }
+
+
         if (child_pid == 0) {
             /* Never returns if the call is successful */
-            execvp(command[0], command);
+            if (execvp(command[0], command) < 0) {
+                perror(command[0]);
+                exit(1);
+            }
         } else {
             waitpid(child_pid, &stat_loc, WUNTRACED);
         }
