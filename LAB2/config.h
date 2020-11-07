0959e943fc1d59d76238e6eda7825fc7b31aad59
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2020-11-07 15:11:34 +0200

    [1] wip colors [2] startup commands

--- LAB2/config.h
+++ LAB2/config.h
@@ -13,8 +13,12 @@
 static const char prompt[] = "{~tsh~}> ";
 
 /*==================================
-  Colors
+  Style
 ==================================*/
+
+#define YES    1
+#define NO    -1
+#define NONE  -1
 
 /* ANSI color codes for defining 256 
  * colors are used. First 8 colors
@@ -24,36 +28,44 @@ static const char prompt[] = "{~tsh~}> ";
  * for further information on ANSI
  * escape codes, see COLORS.md */
 
-/*
------------------
- 8-bit colors
------------------
-* BLACK     0
-* RED       1
-* GREEN     2
-* YELLOW    3
-* BLUE      4
-* MAGENTA   5
-* CYAN      6
-* WHITE     7
-*/
-
-#define YES    1
-#define NO    -1
-#define NONE  -1
-
 static const int col_black    = 0;
 static const int col_red      = 1;
+static const int col_green    = 2;
+static const int col_yellow   = 3;
 static const int col_blue     = 4;
+static const int col_magenta  = 5;
 static const int col_cyan     = 6;
 static const int col_white    = 7;
 
-static const int colors[][5]  = {
-    /*                         fg            bg           bold    underline   blink    */
-    /*                    (color/NONE)  (color/NONE)    (YES/NO)   (YES/NO)  (YES/NO)  */
-    /*                                                                                 */
-    [ColPrompt]       = { col_blue,         NONE,         YES,       NO,       NO      },
-    [ColErrPrefix]    = { col_cyan,         NONE,         YES,       NO,       NO      },
-    [ColErrMsg]       = { col_red,          NONE,         YES,       NO,       NO      }, 
-    [ColErrInput]     = { col_white,        col_black,    NO,        NO,       NO      }, 
+/* make sure to use only the allowed values in parentheses */
+/* if bg color is set, the blink effect will not work      */
+static const int style[][5]  = {
+    /*                        fg              bg           bold    underline   blink    */
+    /*                    (color/NONE)    (color/NONE)   (YES/NO)   (YES/NO)  (YES/NO)  */
+    /*                                                                                  */
+    [StylePrompt]      = { col_blue,         NONE,         YES,       NO,       NO      },
+    [StyleErrPrefix]   = { col_cyan,         NONE,         YES,       NO,       NO      },
+    [StyleErrMsg]      = { col_red,          NONE,         YES,       NO,       NO      }, 
+    [StyleErrInput]    = { col_white,        col_black,    NO,        NO,       NO      }, 
 };
+
+static const char alias_cls[]  = "cls";
+static const char alias_exit[] = "quit";
+static const char alias_ls[]   = "ls --color=auto";
+
+/*==================================
+  Startup
+==================================*/
+
+#define CMD_BUFF_SIZE 256
+
+/* used to track startup array's size */
+static const int startup_cmd_count = 2;
+
+/* startup commands can be added or removed here. 
+ * The number of commands must match "startup_cmd_count" */
+static const char startup[][CMD_BUFF_SIZE] = {
+    "neofetch", 
+    "echo ===============\n" "Welcome to tsh!\n" "===============",
+};
+
--- LAB2/tsh.c
+++ LAB2/tsh.c
@@ -9,38 +9,27 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <setjmp.h>
+/* useful debugging macros */
+#include "dbg.h"
 
+#ifndef CMD_BUFF_SIZE
 #define CMD_BUFF_SIZE 256
-
-char *reset_colors() {
-    return "\x1b[0m";
-}
-
-char *set_colors(int fg, int bg, int bold, int uline, int blink) {
-
-    char *cls = malloc(sizeof(char) * 100);
-
-    // TODO check for out of bounds values
-    uline = (uline == -1) ? uline : uline + 3;
-    blink = (blink == -1) ? blink : blink + 4;
-
-    sprintf(cls, "\x1b[38;5;%dm\x1b[48;5;%dm\x1b[%dm\x1b[%dm\x1b[%dm", fg, bg, bold, uline, blink);
-
-    return cls;
-}
+#endif
 
 /* enums */
-enum { ColPrompt, ColErrPrefix, ColErrMsg, ColErrInput }; /* colors */
-enum { fg, bg, bold, underline, blink };                  /* colors */
+enum { StylePrompt, StyleErrPrefix, StyleErrMsg, StyleErrInput }; /* style */
+enum { fg, bg, bold, uline, blink };                              /* style */
 
-/* configuration */
+/* configuration, allows nested code to access above variables */
 #include "config.h"
 
 char *read_line(const char *);
 char **get_input(char *);
-void clear_screen();
+void clear_screen(int);
 int  cd(char *);
 void sigint_handler(int);
+char *set_style(int, int, int, int, int);
+char *reset_style();
 
 static sigjmp_buf env;
 static volatile sig_atomic_t jump_active = 0;
@@ -50,6 +39,7 @@ int main() {
     char *input;
     pid_t child_pid;
     int stat_loc;
+    int startup_curr_cmd = 0;
 
     errno = 0;
 
@@ -68,8 +58,14 @@ int main() {
 
         jump_active = 1;
 
-        clear_screen();
-        input = read_line(prompt);
+        clear_screen(0);
+
+        if (startup_curr_cmd != startup_cmd_count) {
+            input = strdup(startup[startup_curr_cmd]);
+            startup_curr_cmd++;
+        } else {
+            input = read_line(prompt);
+        }
 
         if (input == NULL) { /* Exit on Ctrl-D */
             printf("exit\n");
@@ -92,8 +88,17 @@ int main() {
             continue;
         }
 
-        if (strcmp(command[0], "exit") == 0) {
+        if (strcmp(command[0], "exit") == 0 ||
+            strcmp(command[0], alias_exit) == 0) {
+
             exit(0);
+        }
+
+        if (strcmp(command[0], alias_cls) == 0) {
+            clear_screen(1);
+
+            // Skip the fork
+            continue;
         }
 
         child_pid = fork();
@@ -111,43 +116,29 @@ int main() {
 
             /* Never returns if the call is successful */
             if (execvp(command[0], command) < 0) {
+
                 fprintf(stderr,
-                        "\x1b[38;5;%dm"
-                        "\x1b[48;5;%dm"
-                        "\x1b[%dm"
-                        "\x1b[%dm"
-                        "\x1b[%dm"
-                        "tsh: "
-                        "\x1b[38;5;%dm"
-                        "\x1b[48;5;%dm"
-                        "\x1b[%dm"
-                        "\x1b[%dm"
-                        "\x1b[%dm"
-                        "%s: "
-                        "\x1b[38;5;%dm"
-                        "\x1b[48;5;%dm"
-                        "\x1b[%dm"
-                        "\x1b[%dm"
-                        "\x1b[%dm"
-                        "%s\n"
-                        "\x1b[0m", 
-                        colors[ColErrPrefix][0],
-                        colors[ColErrPrefix][1],
-                        colors[ColErrPrefix][2],
-                        colors[ColErrPrefix][3],
-                        colors[ColErrPrefix][4],
-                        colors[ColErrMsg][0],
-                        colors[ColErrMsg][1],
-                        colors[ColErrMsg][2],
-                        colors[ColErrMsg][3],
-                        colors[ColErrMsg][4],
+                        "%stsh: %s%s%s: %s%s%s%s\n", 
+                        set_style( style[StyleErrPrefix][fg],
+                                   style[StyleErrPrefix][bg],
+                                   style[StyleErrPrefix][bold],
+                                   style[StyleErrPrefix][uline],
+                                   style[StyleErrPrefix][blink] ),
+                        reset_style(),
+                        set_style( style[StyleErrMsg][fg],
+                                   style[StyleErrMsg][bg],
+                                   style[StyleErrMsg][bold],
+                                   style[StyleErrMsg][uline],
+                                   style[StyleErrMsg][blink] ),
                         strerror(errno),
-                        colors[ColErrInput][0],
-                        colors[ColErrInput][1],
-                        colors[ColErrInput][2],
-                        colors[ColErrInput][3],
-                        colors[ColErrInput][4],
-                        command[0]);
+                        reset_style(),
+                        set_style( style[StyleErrInput][fg],
+                                   style[StyleErrInput][bg],
+                                   style[StyleErrInput][bold],
+                                   style[StyleErrInput][uline],
+                                   style[StyleErrInput][blink] ),
+                        command[0],
+                        reset_style() );
 
                 exit(1);
             }
@@ -171,7 +162,14 @@ char *read_line(const char *prompt) {
 #else
 char *read_line(const char *prompt) {
 
-    printf("%s%s%stest", set_colors(7,-1,1,-1,-1), prompt, reset_colors());
+    printf("%s%s%s", 
+            set_style( style[StylePrompt][fg],
+                        style[StylePrompt][bg],
+                        style[StylePrompt][bold],
+                        style[StylePrompt][uline],
+                        style[StylePrompt][blink] ),
+            prompt,
+            reset_style() );
 
     char *line = malloc(CMD_BUFF_SIZE * sizeof(char));
     if (line == NULL) {
@@ -236,12 +234,20 @@ char **get_input(char *input) {
     return command;
 }
 
-void clear_screen() {
+void clear_screen(int do_it) {
+    if (do_it < 0 || do_it > 1) {
+        printf("invalid argument to clear_screen()");
+        exit(1);
+    }
+
+    const char *CLEAR_SCREEN_ANSI = " \x1b[1;1H\x1b[2J";
     static int first_time = 1; // clear screen for the first time
+
     if (first_time) {
-        const char *CLEAR_SCREEN_ANSI = " \x1b[1;1H\x1b[2J";
-        write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
         first_time = 0;
+        write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
+    } else if (do_it) {
+        write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
     }
 }
 
@@ -256,3 +262,19 @@ void sigint_handler(int signo) {
     siglongjmp(env, 42);
 }
 
+char *set_style(int fg, int bg, int bold, int uline, int blink) {
+
+    char *cls = malloc(sizeof(char) * 100);
+
+    // TODO check for out of bounds values
+    uline = (uline == -1) ? uline : uline + 3;
+    blink = (blink == -1) ? blink : blink + 4;
+
+    sprintf(cls, "\x1b[38;5;%dm\x1b[48;5;%dm\x1b[%dm\x1b[%dm\x1b[%dm", fg, bg, bold, uline, blink);
+
+    return cls;
+}
+
+char *reset_style() {
+    return "\x1b[0m";
+}
